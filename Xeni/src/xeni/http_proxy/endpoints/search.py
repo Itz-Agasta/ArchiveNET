4f0ea475c24f4f4b7fc641b08950131deab46fcf
ZotacMaster <90595737+ZotacMaster@users.noreply.github.com>
2025-06-22 04:23:45 +0530

    Renamed Eva to Xeni (#22)

--- Xeni/src/xeni/http_proxy/endpoints/search.py
+++ Xeni/src/xeni/http_proxy/endpoints/search.py
@@ -1,32 +1,33 @@
-from fastapi import APIRouter, HTTPException, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter
 import os
 import httpx
 import json
+from fastapi import HTTPException
 
-from eva.utils.config import CONFIG_FILE
-from eva.utils.models  import ContextData
+from xeni.utils.config import ConfigManager, EIZEN_URL
+from xeni.utils.models import ContextQuery, ContextResponse
 
 router = APIRouter()
-base_url = os.getenv("BASE_URL", "http://localhost:3000/memories")
+base_url = EIZEN_URL
 headers = {}
-with open(CONFIG_FILE, 'r') as f:
+config = ConfigManager("", "").load_config_path()
+with open(config, 'r') as f:
     config = json.load(f)
     headers["Authorization"] = config.get("Authorization", "")
     headers["x-contract-id"] = config.get("x-contract-id", "")
     headers["Content-Type"] = "application/json"
-# Create a new client and connect to the server
 
-@router.post("/context/insert")
-async def insert_context(data:ContextData)-> JSONResponse:
+@router.post("/context/search")
+async def search_context(query: ContextQuery)-> ContextResponse:
+    print(query.model_dump(mode="json"))
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            f"{base_url}/insert",
-            json= data.model_dump(mode="json"),
+            f"{base_url}/search",
+            json= query.model_dump(mode="json"),
             headers = headers
         )
     if response.status_code == 200:
-        return JSONResponse(content=response.json())
+        return ContextResponse(**response.json())
     else:
         raise HTTPException(
             status_code=response.status_code,
